4cc546ed3405344d98a822adb70acd9b25c68826
alexandra-zaharia <7894916+alexandra-zaharia@users.noreply.github.com>
2021-11-07 19:05:23 +0100

    add code for color enum for terminal color with ansi escape codes

---
+++ color_enum/README.md
@@ -0,0 +1,3 @@
+# Color enum class for ANSI color codes
+
+This is the code accompanying my blog post [Python color enum class for ANSI color codes](https://alexandra-zaharia.github.io/posts/python-color-enum-class-for-ansi-color-codes).
---
+++ color_enum/color.py
@@ -0,0 +1,45 @@
+from enum import Enum, auto
+
+
+class Color(Enum):
+    RED = 31
+    GREEN = auto()
+    YELLOW = auto()
+    BLUE = auto()
+    MAGENTA = auto()
+    CYAN = auto()
+    LIGHTRED = 91
+    LIGHTGREEN = auto()
+    LIGHTYELLOW = auto()
+    LIGHTBLUE = auto()
+    LIGHTMAGENTA = auto()
+    LIGHTCYAN = auto()
+
+    _START = '\u001b['
+    _BOLD = ';1'
+    _BLINK = ';5'
+    _END = 'm'
+    _RESET = '\u001b[0m'
+
+    @staticmethod
+    def colored(color, msg, bold=False, blink=False):
+        if not(isinstance(color, Color)):
+            raise TypeError(f'Unknown color {color}')
+
+        fmt_msg = Color._START.value + str(color.value)
+
+        if bold:
+            fmt_msg += Color._BOLD.value
+        if blink:
+            fmt_msg += Color._BLINK.value
+
+        return fmt_msg + Color._END.value + str(msg) + Color._RESET.value
+
+
+if __name__ == '__main__':
+    for item in Color:
+        if item.name.startswith('_'):
+            continue
+        print(Color.colored(item, item.name))
+        if item.name.startswith('LIGHT'):
+            print(Color.colored(item, f'{item.name[5:]} bold == {item.name} bold', bold=True))
